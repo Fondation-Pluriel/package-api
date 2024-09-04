de7b2f573e2ba0740079738d5ae85af72e85eddc
EstebanRacine <racine.esteban@lyceepergaud.fr>
2024-09-04 10:56:32 +0200

    Composer + YAML + Gestion JWT

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use App\Entity\User;
+use App\Security\JWTTokenManager;
+
+class ApiController extends AbstractController
+{
+    private $passwordHasher;
+    private $jwtManager;
+    private $jwtTokenManager;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $jwtManager, JWTTokenManager $jwtTokenManager)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->jwtManager = $jwtManager;
+        $this->jwtTokenManager = $jwtTokenManager;
+    }
+
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function login(Request $request, UserRepository $userRepository): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $username = $data['username'] ?? '';
+        $password = $data['password'] ?? '';
+
+        $user = $userRepository->findOneBy(['username' => $username]);
+
+        if(!$user || !$this->passwordHasher->isPasswordValid($user, $password)) {
+            return new JsonResponse(['error' => 'Invalid credentials'], Response::HTTP_UNAUTHORIZED);
+        }
+        $jwt = $this->jwtTokenManager->create($user);
+        return new JsonResponse(['token' => $jwt]);
+    }
+
+    // #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    // public function login(Request $request, UserRepository $userRepository): JsonResponse
+    // {
+    //     $data = json_decode($request->getContent(), true);
+    //     $username = $data['username'] ?? '';
+    //     $password = $data['password'] ?? '';
+
+    //     $user = $userRepository->findOneBy(['username' => $username]);
+
+    //     if (!$user || !$this->passwordHasher->isPasswordValid($user, $password)) {
+    //         return new JsonResponse(['error' => 'Invalid credentials'], Response::HTTP_UNAUTHORIZED);
+    //     }
+
+    //     $token = $this->jwtManager->create($user);
+
+    //     return new JsonResponse(['token' => $token]);
+    // }
+}
---
+++ src/Security/JWTTokenManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Security;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class JWTTokenManager
+{
+    private $jwtManager;
+
+    public function __construct(JWTTokenManagerInterface $jWTTokenManagerInterface)
+    {
+        $this->jwtManager = $jWTTokenManagerInterface;
+    }
+
+    public function create(UserInterface $user): string
+    {
+        $roles = $user->getRoles();
+        $expiration = $this->getExpirationByRoles($roles);
+
+        $payload = ['exp' => time() + $expiration];
+        return $this->jwtManager->createFromPayload($user, $payload);
+    }
+
+    private function getExpirationByRoles(array $roles): int
+    {
+        if (in_array('ROLE_WEBAPP', $roles, true)) {
+            return 129600; // 1.5 jours pour le web service
+        }
+
+        if (in_array('ROLE_QLIKSENS', $roles, true)) {
+            return 31536000; // 1 an pour les utilisateurs QlikSens (droit en lecture seul)
+        }
+
+        // Durée par défaut si aucun rôle spécifique n'est trouvé
+        return 3600; // 1 heure par défaut
+    }
+}
